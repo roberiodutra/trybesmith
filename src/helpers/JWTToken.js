57e3435f85adb474625e843769a6ae8afb4d24bd
roberio <roberio-almeida@hotmail.com>
2022-08-02 05:05:49 -0300

    chore: add jwt token helper file

---
+++ src/helpers/JWTToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const SECRET = process.env.JWT_SECRET;
+
+const jwtConfig = {
+  expiresIn: '15m',
+  algorithm: 'HS256',
+};
+
+const generateToken = (payload) =>
+  jwt.sign(payload, SECRET, jwtConfig);
+
+module.exports = generateToken;
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 const httpStatus = require('./httpStatusCodes');
 const errorMessages = require('./errorMessages');
+const generateToken = require('./JWTToken');
 
-module.exports = { httpStatus, errorMessages };
+module.exports = { httpStatus, errorMessages, generateToken };
