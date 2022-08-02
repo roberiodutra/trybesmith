b7fc56e7cf638a4e01bd0e54625715de9e7b6bc5
roberio <roberio-almeida@hotmail.com>
2022-08-02 04:32:48 -0300

    chore: add httpStatusCodes helper file

---
+++ src/helpers/httpStatusCodes.js
@@ -0,0 +1,13 @@
+const httpStatusCodes = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  UNPROCESSABLE: 422,
+  INTERNAL_SERVER: 500,
+};
+
+module.exports = httpStatusCodes;
