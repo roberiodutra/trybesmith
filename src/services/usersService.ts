1fa9777341395d0f638940ce88cc8b80c179de42
roberio <roberio-almeida@hotmail.com>
2022-08-03 03:36:23 -0300

    feat: create users service

---
+++ src/services/usersService.ts
@@ -0,0 +1,9 @@
+import * as usersModel from '../models/usersModel';
+import IUsers from '../interfaces/IUsers';
+import { generateToken } from '../helpers/JWTToken';
+
+export const create = async (user: IUsers): Promise<{ token: string }> => { 
+  await usersModel.create(user);
+  const token = generateToken(user);
+  return { token };
+};
