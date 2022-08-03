9bbe2244b65168c7288600de4a38638b96e648a2
roberio <roberio-almeida@hotmail.com>
2022-08-03 06:41:06 -0300

    fix: some fix

--- src/interfaces/IOrders.ts
+++ src/interfaces/IOrders.ts
@@ -1,5 +1,5 @@
 export interface IOrders {
   id: number;
   userId: number;
-  productsIds: number[];
+  productsIds: string;
 }
--- src/services/ordersService.ts
+++ src/services/ordersService.ts
@@ -1,11 +1,10 @@
 import * as ordersModel from '../models/ordersModel';
-import { IOrders } from '../interfaces/IOrders';
 
-export const getAll = async (): Promise<IOrders[]> => {
+export const getAll = async () => {
   const ordersIds = await ordersModel.getAll();
 
-  return ordersIds.map((item: any) => {
-    const productsIds = item.productsIds.split(',').map(Number)
-    return { ...item, productsIds }
+  return ordersIds.map((item) => {
+    const productsIds = item.productsIds.split(',').map(Number);
+    return { ...item, productsIds };
   });
 };
