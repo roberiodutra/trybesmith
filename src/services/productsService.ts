a7fbd7f5d4d129a38a30a476aecb37b14c417a6d
roberio <roberio-almeida@hotmail.com>
2022-08-03 03:01:10 -0300

    feat: create getAll products service

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -5,3 +5,8 @@ export const create = async (product: IProducts): Promise<IProducts> => {
   const createdProduct = await productsModel.create(product);
   return createdProduct;
 };
+
+export const getAll = async (): Promise<IProducts[]> => {
+  const products = productsModel.getAll()
+  return products;
+};
