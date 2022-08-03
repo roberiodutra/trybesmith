3142cc7035932bcff5d75755ed6ec355b59ea2ab
roberio <roberio-almeida@hotmail.com>
2022-08-03 03:34:57 -0300

    feat: create users model

--- src/helpers/JWTToken.ts
+++ src/helpers/JWTToken.ts
@@ -1,15 +1,14 @@
-// import { sign, SignOptions, Secret } from 'jsonwebtoken';
-// import dotenv from 'dotenv';
-// dotenv.config();
+import { sign, SignOptions, Secret } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import IUsers from '../interfaces/IUsers';
+dotenv.config();
 
-// const SECRET: Secret = process.env.JWT_SECRET || 'vnetod';
+const SECRET: Secret = process.env.JWT_SECRET || 'vnetod';
 
-// const jwtConfig: SignOptions = {
-//   expiresIn: '15m',
-//   algorithm: 'HS256',
-// };
+const jwtConfig: SignOptions = {
+  expiresIn: '15m',
+  algorithm: 'HS256',
+};
 
-// const generateToken = (payload) =>
-//   sign(payload, SECRET, jwtConfig);
-
-// export default generateToken;
+export const generateToken = (payload: IUsers) =>
+  sign(payload, SECRET, jwtConfig);
--- src/models/usersModel.ts
+++ src/models/usersModel.ts
@@ -1,7 +1,8 @@
 import { ResultSetHeader } from 'mysql2/promise';
+import IUsers from '../interfaces/IUsers';
 import connection from './connection';
 
-export const create = async (u): Promise<void> => {
+export const create = async (u: IUsers): Promise<void> => {
   const query = `INSERT INTO Trybesmith.Users
   ( username, classe, level, password ) VALUES (?, ?, ?, ?)`;
   const values = [u.username, u.classe, u.level, u.password];
