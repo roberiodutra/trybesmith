783c14390fe23a09034ef128be8bde005d6edbc2
roberio <roberio-almeida@hotmail.com>
2022-08-05 19:43:54 -0300

    chore: config create orders route

--- src/routers/ordersRouter.ts
+++ src/routers/ordersRouter.ts
@@ -4,5 +4,6 @@ import ordersController from '../controllers/ordersController';
 const ordersRouter = express.Router();
 
 ordersRouter.get('/', ordersController.getAll);
+ordersRouter.create('/', ordersController.create);
 
 export default ordersRouter;
