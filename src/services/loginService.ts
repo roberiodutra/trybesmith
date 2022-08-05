c886ac465a011d1147048c7060b37e26ceb987c9
roberio <roberio-almeida@hotmail.com>
2022-08-05 04:04:47 -0300

    feat: create login service

---
+++ src/services/loginService.ts
@@ -0,0 +1,11 @@
+import { getUser } from '../models/usersModel';
+import { generateToken } from '../helpers/JWTToken';
+import IUsers from '../interfaces/IUsers';
+
+const login = async (body: IUsers): Promise<string> => {
+  const userExists = await getUser(body);
+  const token = generateToken(body);
+  return token;
+};
+
+export default { login };
