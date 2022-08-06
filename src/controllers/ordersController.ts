f8c00153db210ecf740b880fda3e813568779375
roberio <roberio-almeida@hotmail.com>
2022-08-06 04:42:56 -0300

    feat: create order controller

--- src/controllers/ordersController.ts
+++ src/controllers/ordersController.ts
@@ -1,11 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
 import * as ordersService from '../services/ordersService';
 import httpStatus from '../helpers/httpStatusCodes';
+import validateTokenService from '../services/validateTokenService';
+
+export interface IReqUser extends Request {
+  user?: any,
+};
 
 export const getAll = async (_req: Request, res: Response, next: NextFunction) => {
   try {
     const orders = await ordersService.getAll();
     return res.status(httpStatus.OK).json(orders);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const create = async (req: IReqUser, res: Response, next: NextFunction): Promise<Response | void> => {
+  try {
+    await validateTokenService.validate(req);
+    const created = await ordersService.create(req.user.id, req.body.productsIds);
+    return res.status(httpStatus.CREATED).json(created);
   } catch (err) {
     next(err);
   }
