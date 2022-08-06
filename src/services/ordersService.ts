5a6ce1a3d653e3eb2b679846616de03f355f14aa
roberio <roberio-almeida@hotmail.com>
2022-08-06 04:38:10 -0300

    feat: create order service

--- src/services/ordersService.ts
+++ src/services/ordersService.ts
@@ -1,4 +1,6 @@
 import * as ordersModel from '../models/ordersModel';
+import * as productsModel from '../models/productsModel';
+import schemas from '../schemas';
 
 export const getAll = async () => {
   const orders = await ordersModel.getAll();
@@ -7,4 +9,17 @@ export const getAll = async () => {
     const productsIds = item.productsIds.split(',').map(Number);
     return { ...item, productsIds };
   });
+};
+
+export const create = async (userId: number, productsIds: number[]) => {
+
+  const orderId = await ordersModel.create(userId);
+
+  await Promise.all(
+    productsIds.map(async(product) => (
+      await productsModel.update(orderId, product)
+    )),
+  );
+
+  return { userId, productsIds };
 };
